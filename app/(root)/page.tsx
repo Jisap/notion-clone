203d9265dfa68a410fa630cfffd2ec4805d3eb10
Jorge Matos <jisapjisap@gmail.com>
2023-10-16 20:15:53 +0200

    folders setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
+  return ( 
     <div>
       <p className='text-red-500 font-bold'>Hello Notion Clone</p>
       <Button>
