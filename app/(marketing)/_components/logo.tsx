04869489585add47fb4ba2d8684f464cff4aa83f
Jorge Matos <jisapjisap@gmail.com>
2023-10-17 23:37:53 +0200

    landing page

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import { cn } from "@/lib/utils"
+import { Poppins } from "next/font/google"
+
+
+const font = Poppins({
+   subsets: ["latin"],
+   weight: ["400", "600"]
+});
+
+const Logo = () => {
+   return (
+      <div className="hidden md:flex items-center gap-x-2">
+         <Image 
+            src="/logo.svg"
+            height="40"
+            width="40"
+            alt="Logo"
+         />
+         <p className={cn("font-semibold", font.className)}>
+            Jotion
+         </p>
+      </div>
+   )
+}
+
+export default Logo
