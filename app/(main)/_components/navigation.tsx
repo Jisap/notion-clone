00d8027b40daaccbc06416c98b3b145a5198ae4d
Jorge Matos <jisapjisap@gmail.com>
2023-10-23 19:51:07 +0200

    navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon } from "lucide-react"
 import { usePathname } from "next/navigation";
-import { ElementRef, useRef, useState } from "react"
+import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts";
 
 
@@ -19,13 +21,28 @@ const Navigation = () => {
    const[isResetting, setIsResetting] = useState(false);
    const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+
+   useEffect(() => {
+      if (isMobile) {
+         collapse();
+      } else {
+         resetWidth();
+      }
+   }, [isMobile]);
+
+   useEffect(() => {
+      if (isMobile) {
+         collapse();
+      }
+   }, [pathname, isMobile]);
+
    const handleMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
       event.preventDefault();
       event.stopPropagation();
 
       isResizingRef.current = true;
-      document.addEventListener("mousemove", handleMouseMove)
-      document.addEventListener("mouseup", handleMouseUp)
+      document.addEventListener("mousemove", handleMouseMove) // Cambia el ancho del sidebar y el navbar
+      document.addEventListener("mouseup", handleMouseUp)     // Se deja de escuchar los cambios de ancho 
    };
 
    const handleMouseMove = (event:MouseEvent) => { // Se mueve el ratón
@@ -48,6 +65,37 @@ const Navigation = () => {
       document.removeEventListener("mouseup", handleMouseUp);
    };
 
+   const resetWidth = () => { // Se llama en pantallas de !isMobile, click separador, y icon de isMobile
+
+      if( sidebarRef.current && navbarRef.current){
+         setIsCollapsed(false);
+         setIsResetting(true);
+
+         sidebarRef.current.style.width = isMobile ? "100%" : "240px"
+         navbarRef.current.style.setProperty(
+            "width",
+            isMobile ? "0" : "calc(100% -240px)"   // Si estamos mobile navbar tendrá width=0 -> se oculta, sino 100% - 240px
+         );
+         navbarRef.current.style.setProperty(      
+            "left",
+            isMobile ? "100%" : "240px"            // Si estamos en mobile el margen izquierdo se pone al 100% -> se oculta 
+         );
+         setTimeout(() => setIsResetting(false), 300);
+      }
+   }
+
+   const collapse = () => {
+      if (sidebarRef.current && navbarRef.current) {
+         setIsCollapsed(true);
+         setIsResetting(true);
+
+         sidebarRef.current.style.width = "0";
+         navbarRef.current.style.setProperty("width", "100%");
+         navbarRef.current.style.setProperty("left", "0");
+         setTimeout(() => setIsResetting(false), 300);
+      }
+   }
+
    return (
       <>
          <aside
@@ -60,6 +108,7 @@ const Navigation = () => {
          >
             <div
                role="button"
+               onClick={collapse}
                className={cn("h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
                isMobile && "opacity-100"
                )}
@@ -76,7 +125,7 @@ const Navigation = () => {
             {/* Barra de separación entre sidebarRef y navbarRef */}
             <div 
                onMouseDown={handleMouseDown}
-               onClick={() => {}}
+               onClick={resetWidth}
                className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize
                absolute h-full w-1 bg-primary/10 right-0 top-0"
             />     
@@ -91,7 +140,7 @@ const Navigation = () => {
             )}
          >
             <nav className="bg-transparent px-3 py-2 w-full">
-               {isCollapsed && <MenuIcon className="h-6 w-6 text-muted-foreground" />}
+               {isCollapsed && <MenuIcon className="h-6 w-6 text-muted-foreground" onClick={resetWidth}/>}
             </nav>
          </div>
       </>
