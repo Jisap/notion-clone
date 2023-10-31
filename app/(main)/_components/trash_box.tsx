4f8b2c00c502fa92fb77767af5f2af07fdc5a1b2
Jorge Matos <jisapjisap@gmail.com>
2023-10-31 18:20:10 +0100

    TrashBox

--- app/(main)/_components/trash_box.tsx
+++ app/(main)/_components/trash_box.tsx
@@ -9,7 +9,7 @@ import { toast } from "sonner";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { Spinner } from "@/components/spinner";
-//import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
 //import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 export const TrashBox = () => {
@@ -68,8 +68,45 @@ export const TrashBox = () => {
 
 
   return (
-    <div>
-      Trash Box
+    <div className="text-sm">
+      <div className="flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="Filter by page title..."
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        {/* Este parrafo por defecto oculto y solo se mostrará si no hay contenido en los child que vienen ahora */}
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+          No documents found.
+        </p>
+        {
+          filteredDocuments?.map((document) => (
+            <div
+              key={document._id}
+              role="button"
+              onClick={() => onClick(document._id)}
+              className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
+            >
+              <span className="truncate pl-2">
+                {document.title}
+              </span>
+              <div className="flex items-center">
+                <div
+                  onClick={(e) => onRestore(e, document._id)}
+                  role="button"
+                  className="rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600"
+                >
+                  <Undo className="h-4 w-4 text-muted-foreground" />
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
