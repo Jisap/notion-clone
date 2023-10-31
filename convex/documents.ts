7a5f8cecf4a063cc380c19d2b6d78e7cb9500290
Jorge Matos <jisapjisap@gmail.com>
2023-10-31 17:54:21 +0100

    TrashBox

--- convex/documents.ts
+++ convex/documents.ts
@@ -200,3 +200,30 @@ export const restore = mutation({
         return document;    //  Finalmente se devuelve el documento principal actualizado como resultado de la operación de restauración.
     }
 });
+
+export const remove = mutation({
+    args: { id: v.id("documents") },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const existingDocument = await ctx.db.get(args.id);
+
+        if (!existingDocument) {
+            throw new Error("Not found");
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const document = await ctx.db.delete(args.id);
+
+        return document;
+    }
+});
