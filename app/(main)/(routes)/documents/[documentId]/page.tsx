8aa20dface026547d9d37183803c2d239cbbda44
Jorge Matos <jisapjisap@gmail.com>
2023-11-06 23:27:23 +0100

    [documentId]

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,49 @@
+"use client";
 
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
-const DocumentIdPage = () => {
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+//import { Toolbar } from "@/components/toolbar";
+//import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+};
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps ) => {
+
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  });
+
+  if( document === undefined ){
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if(document === null){
+    return (
+      <div>
+        Not found
+      </div>  
+    )
+  }
+
   return (
-    <div>
-      DocumentIdPage
+    <div className="pb-40">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document}/>
+      </div>
     </div>
   )
 }
